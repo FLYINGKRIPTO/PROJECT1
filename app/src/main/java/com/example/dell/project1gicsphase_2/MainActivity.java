98cf596979568924178003741dc5eb7ffc2532ad
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2018-06-05 09:54:14 +0530

    deleted layout-v16 and modified MainActivity.java changed fonts

--- app/src/main/java/com/example/dell/project1gicsphase_2/MainActivity.java
+++ app/src/main/java/com/example/dell/project1gicsphase_2/MainActivity.java
@@ -11,16 +11,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = (TextView) findViewById(R.id.musicgallery);
+        TextView textView =  findViewById(R.id.musicgallery);
         Typeface typeface1 = Typeface.createFromAsset(getAssets(), "fonts/iconmoon.ttf");
         textView.setTypeface(typeface1);
 
 
-     /*   TextView textView2 = (TextView) findViewById(R.id.address1);
+        TextView textView2 = findViewById(R.id.address1);
         textView2.setTypeface(typeface1);
         TextView textView3 = findViewById(R.id.address2);
         textView3.setTypeface(typeface1);
-        TextView textView4 = (TextView) findViewById(R.id.contact);
-        textView4.setTypeface(typeface1); */
+        TextView textView4 = findViewById(R.id.contact);
+        textView4.setTypeface(typeface1);
+        TextView textView5 = findViewById(R.id.guitar);
+        textView5.setTypeface(typeface1);
+        TextView textView6 = findViewById(R.id.harmonium);
+        textView6.setTypeface(typeface1);
+        TextView textView7 = findViewById(R.id.tabla);
+        textView7.setTypeface(typeface1);
+        TextView textView8 = findViewById(R.id.flute);
+        textView8.setTypeface(typeface1);
+        TextView textView9 = findViewById(R.id.keyboard);
+        textView9.setTypeface(typeface1);
+        TextView textView10 = findViewById(R.id.drums);
+        textView10.setTypeface(typeface1);
     }
 }
