aa8499b2c8172a3dc46792d4a6681164a7cfe3cd
FLYINGKRIPTO <srivastava.shivanshu4@gmail.com>
2018-06-05 19:49:40 +0530

    made different layouts for both landscape and portrait mode used dimens.xml to specify dimensions

--- app/src/main/java/com/example/dell/project1gicsphase_2/MainActivity.java
+++ app/src/main/java/com/example/dell/project1gicsphase_2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.dell.project1gicsphase_2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+        public void process(View view){
 
+            Intent intent= null;
+
+            if(view.getId()==R.id.address1||view.getId()==R.id.address2){
+                intent= new Intent( Intent.ACTION_VIEW );
+                intent.setData( Uri.parse("geo:23.2258931,77.4359399"));
+                startActivity(intent);
+
+            }
     }
 }
